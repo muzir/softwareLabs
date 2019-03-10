3283aa89dd3cd71be46122baa0407ddfa901dcd4
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-03-10 08:41:29 +0200

    Working on docker-compose support

--- spring-boot-testcontainers/src/main/java/com/softwarelabs/product/ProductInDb.java
+++ spring-boot-testcontainers/src/main/java/com/softwarelabs/product/ProductInDb.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @AllArgsConstructor
 @Entity
 public class ProductInDb implements Product{
+	@Id
 	private Long id;
 	private String name;
 }
