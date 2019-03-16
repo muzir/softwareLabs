b8c12eeb878048359d8db7f581d2692b413f3223
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-03-16 20:24:51 +0200

    Delete unnecessary configuration

--- spring-boot-testcontainers/src/test/resources/db/embedded-postgres-init.sql
+++ spring-boot-testcontainers/src/test/resources/db/embedded-postgres-init.sql
@@ -1,22 +1,9 @@
-CREATE USER dbuser WITH PASSWORD 'password';
-
+-- noinspection SqlNoDataSourceInspectionForFile
 
 CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START 1;
-
--- Set permission to database user for sequences.
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO dbuser;
-
--- Set permission to database user for schema.
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-  GRANT USAGE, SELECT ON SEQUENCES TO dbuser;
-
-
 
 create table if not exists product
 (
   id  bigint not null constraint product_pkey primary key,
   name  varchar(255) UNIQUE
 );
-
--- Set permission to database user for tables.
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dbuser;
