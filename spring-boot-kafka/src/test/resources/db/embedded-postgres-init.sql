96a755a52754556a8145d02a7357e617f8c9a58e
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-04-07 16:48:30 +0300

    Set id value as 0, because hibernate sequence is also started from 1.

--- spring-boot-kafka/src/test/resources/db/embedded-postgres-init.sql
+++ spring-boot-kafka/src/test/resources/db/embedded-postgres-init.sql
@@ -4,9 +4,11 @@ CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START 1;
 
 create table if not exists product
 (
-  id  bigint not null constraint product_pkey primary key,
-  price NUMERIC (5, 2),
+  id    bigint not null
+    constraint product_pkey primary key,
+  price NUMERIC(5, 2),
   name  varchar(255) UNIQUE
 );
 
-INSERT INTO product (id, price, name) values (1, 0, 'product1');
+INSERT INTO product (id, price, name)
+values (0, 0, 'product1');
