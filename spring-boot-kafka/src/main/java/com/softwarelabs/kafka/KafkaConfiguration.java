64df68d2a8ce70c9df02eb3119d1d260ade506b0
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-04-21 13:49:05 +0300

    Producer config naming.

--- spring-boot-kafka/src/main/java/com/softwarelabs/kafka/KafkaConfiguration.java
+++ spring-boot-kafka/src/main/java/com/softwarelabs/kafka/KafkaConfiguration.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.consumer.OffsetCommitCallback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -40,13 +41,13 @@ public class KafkaConfiguration {
 	public Producer<String, String> kafkaProducer(
 			@Value("${kafka.bootstrap.servers}") String bootstrapServers) {
 		final Properties props = new Properties();
-		props.put("bootstrap.servers", bootstrapServers);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("acks", "all");
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("clientId", "productProducer");
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "productProducer");
 		return new KafkaProducer<>(props);
 	}
 
