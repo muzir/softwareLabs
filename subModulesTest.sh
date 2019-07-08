41bc973b307e24b9d4b30b3154a8eb0b83676407
Erhun Baycelik <erhun.baycelik@transferwise.com>
2019-07-08 10:08:07 +0300

    Upgrade gradle 5.

--- subModulesTest.sh
+++ subModulesTest.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 cd spring-boot-integration-test || exit
 echo "Run tests in spring-boot-integration-test"
-gradle test
+./gradlew test
 cd .. || exit
 cd spring-boot-containers || exit
 echo "Run tests in spring-boot-containers"
-gradle test
+./gradlew test
 cd .. || exit
 cd spring-boot-kafka || exit
 echo "Run tests in spring-boot-kafka"
-gradle test
+./gradlew test
 cd .. || exit
 cd spring-boot-kafka-cluster || exit
 echo "Run tests in spring-boot-kafka-cluster"
-gradle test
+./gradlew test
