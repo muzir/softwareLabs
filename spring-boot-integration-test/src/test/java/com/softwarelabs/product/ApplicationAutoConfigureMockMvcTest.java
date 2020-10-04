da2c7082e3f9c34331e423e114ee5526215e602a
Erhun Baycelik <erhun.baycelik@transferwise.com>
2020-10-04 08:52:07 +0300

    Test Ci failing

--- spring-boot-integration-test/src/test/java/com/softwarelabs/product/ApplicationAutoConfigureMockMvcTest.java
+++ spring-boot-integration-test/src/test/java/com/softwarelabs/product/ApplicationAutoConfigureMockMvcTest.java
@@ -45,7 +45,7 @@ public class ApplicationAutoConfigureMockMvcTest {
 	public void createProductReturnHttpStatusCode200_ifProductIsValid() throws Exception {
 		String productName = "Product-1";
 		IProductPort.ProductRequest productRequest =
-				new IProductPort.ProductRequest().setId(productId).setName("P");
+				new IProductPort.ProductRequest().setId(productId).setName(productName);
 		String json = objectMapper.writeValueAsString(productRequest);
 
 		this.mockMvc
