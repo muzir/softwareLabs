06a3bc5cfa7aec2bdc1b9d570a71c9d62ee55f98
Erhun Baycelik <erhun.baycelik@transferwise.com>
2021-04-22 00:08:33 +0300

    - Change some type and null checks.

--- database-indexing/imdb.sql
+++ database-indexing/imdb.sql
@@ -30,8 +30,8 @@ select tnb.nconst,
        tnb.primaryName,
        CASE WHEN tnb.birthYear = '\N' THEN NULL ELSE tnb.birthYear :: Integer END,
        CASE WHEN tnb.deathYear = '\N' THEN NULL ELSE tnb.deathYear :: Integer END,
-       string_to_array(tnb.primaryProfession, ','),
-       string_to_array(tnb.knownForTitles, ',')
+       CASE WHEN tnb.primaryProfession = '\N' THEN NULL ELSE string_to_array(tnb.primaryProfession, ',') END,
+       CASE WHEN tnb.knownForTitles = '\N' THEN NULL ELSE string_to_array(tnb.knownForTitles, ',') END
 from temp_name_basics tnb;
 
 drop table if exists temp_name_basics;
@@ -68,29 +68,35 @@ create table title_akas
 );
 
 insert
-into
-    title_akas (titleId,
-                ordering,
-                title,
-                region,
-                language,
-                types ,
-                attributes,
-                isOriginalTitle)
-select
-    tta.titleId,
-    tta.ordering,
-    tta.title,
-    tta.region,
-    tta.language,
-    string_to_array(tta.types, ','),
-    string_to_array(tta.attributes, ','),
-    case
-        when tta.isOriginalTitle = '\N' then null
-        else tta.isOriginalTitle :: boolean
-        end
-from
-    temp_title_akas tta;
+into title_akas (titleId,
+                 ordering,
+                 title,
+                 region,
+                 language,
+                 types,
+                 attributes,
+                 isOriginalTitle)
+select tta.titleId,
+       tta.ordering,
+       tta.title,
+       tta.region,
+       case
+           when tta.language = '\N' then null
+           else tta.language
+           end,
+       case
+           when tta.types = '\N' then null
+           else string_to_array(tta.types, ',')
+           end,
+       case
+           when tta.attributes = '\N' then null
+           else string_to_array(tta.attributes, ',')
+           end,
+       case
+           when tta.isOriginalTitle = '\N' then null
+           else tta.isOriginalTitle :: boolean
+           end
+from temp_title_akas tta;
 
 drop table if exists temp_title_akas;
 
@@ -113,19 +119,21 @@ create table temp_title_basics
 COPY temp_title_basics FROM '/tmp/title.basics.tsv' DELIMITER E'\t' CSV HEADER QUOTE E'\b' NULL AS '';
 
 drop table if exists title_basics;
-create table title_basics (tconst text,
-                           titleType text,
-                           primaryTitle text,
-                           originalTitle text,
-                           isAdult boolean,
-                           startYear integer,
-                           endYear integer,
-                           runtimeMinutes integer,
-                           genres text[]);
+create table title_basics
+(
+    tconst         text,
+    titleType      text,
+    primaryTitle   text,
+    originalTitle  text,
+    isAdult        boolean,
+    startYear      integer,
+    endYear        integer,
+    runtimeMinutes integer,
+    genres         text[]
+);
 
 insert
-into
-    title_basics( tconst,
+into title_basics(tconst,
                   titleType,
                   primaryTitle,
                   originalTitle,
@@ -133,28 +141,26 @@ into
                   startYear,
                   endYear,
                   runtimeMinutes,
-                  genres )
-select
-    ttb.tconst ,
-    ttb.titletype ,
-    ttb.primarytitle ,
-    ttb.originaltitle ,
-    ttb.isadult,
-    case
-        when ttb.startyear = '\N' then null
-        else ttb.startyear :: Integer
-        end,
-    case
-        when ttb.endyear = '\N' then null
-        else ttb.endyear :: Integer
-        end,
-    case
-        when ttb.runtimeMinutes = '\N' then null
-        else ttb.runtimeMinutes :: Integer
-        end,
-    string_to_array(ttb.genres, ',')
-from
-    temp_title_basics ttb;
+                  genres)
+select ttb.tconst,
+       ttb.titletype,
+       ttb.primarytitle,
+       ttb.originaltitle,
+       ttb.isadult,
+       case
+           when ttb.startyear = '\N' then null
+           else ttb.startyear :: Integer
+           end,
+       case
+           when ttb.endyear = '\N' then null
+           else ttb.endyear :: Integer
+           end,
+       case
+           when ttb.runtimeMinutes = '\N' then null
+           else ttb.runtimeMinutes :: Integer
+           end,
+       string_to_array(ttb.genres, ',')
+from temp_title_basics ttb;
 
 drop table if exists temp_title_basics;
 
@@ -190,10 +196,13 @@ drop table if exists temp_title_crew;
 
 drop table if exists temp_title_episode;
 
-create table temp_title_episode ( tconst text,
-                                  parentTconst text,
-                                  seasonNumber text,
-                                  episodeNumber text );
+create table temp_title_episode
+(
+    tconst        text,
+    parentTconst  text,
+    seasonNumber  text,
+    episodeNumber text
+);
 
 copy temp_title_episode
     from
@@ -201,78 +210,81 @@ copy temp_title_episode
 
 drop table if exists title_episode;
 
-create table title_episode ( tconst text,
-                             parentTconst text,
-                             seasonNumber integer,
-                             episodeNumber integer );
+create table title_episode
+(
+    tconst        text,
+    parentTconst  text,
+    seasonNumber  integer,
+    episodeNumber integer
+);
 
 insert
-into
-    title_episode (tconst,
-                   parentTconst,
-                   seasonNumber,
-                   episodeNumber)
-select
-    tte.tconst,
-    tte.parentTconst,
-    case
-        when tte.seasonNumber = '\N' then null
-        else tte.seasonNumber :: Integer
-        end,
-    case
-        when tte.episodeNumber = '\N' then null
-        else tte.episodeNumber :: Integer
-        end
-from
-    temp_title_episode tte;
+into title_episode (tconst,
+                    parentTconst,
+                    seasonNumber,
+                    episodeNumber)
+select tte.tconst,
+       tte.parentTconst,
+       case
+           when tte.seasonNumber = '\N' then null
+           else tte.seasonNumber :: Integer
+           end,
+       case
+           when tte.episodeNumber = '\N' then null
+           else tte.episodeNumber :: Integer
+           end
+from temp_title_episode tte;
 
 drop table if exists temp_title_episode;
 
 -- title_principals
 drop table if exists temp_title_principals;
 
-create table temp_title_principals ( tconst text,
-                                     ordering integer,
-                                     nconst text,
-                                     category text,
-                                     job text,
-                                     characters text );
+create table temp_title_principals
+(
+    tconst     text,
+    ordering   integer,
+    nconst     text,
+    category   text,
+    job        text,
+    characters text
+);
 
 copy temp_title_principals
     from
     '/tmp/title.principals.tsv' delimiter E'\t' csv header quote E'\b' null as '';
 
 drop table if exists title_principals;
-create table title_principals ( tconst text,
-                                ordering integer,
-                                nconst text,
-                                category text,
-                                job text,
-                                characters text );
+create table title_principals
+(
+    tconst     text,
+    ordering   integer,
+    nconst     text,
+    category   text,
+    job        text,
+    characters text
+);
 
 insert
-into
-    title_principals (tconst,
-                      ordering,
-                      nconst,
-                      category,
-                      job,
-                      characters)
-select
-    ttp.tconst,
-    ttp.ordering,
-    ttp.nconst,
-    ttp.category,
-    case
-        when ttp.job = '\N' then null
-        else ttp.job
-        end,
-    case
-        when ttp."characters" = '\N' then null
-        else ttp."characters"
-        end
-from
-    temp_title_principals ttp;
+into title_principals (tconst,
+                       ordering,
+                       nconst,
+                       category,
+                       job,
+                       characters)
+select ttp.tconst,
+       ttp.ordering,
+       ttp.nconst,
+       ttp.category,
+       case
+           when ttp.job = '\N' then null
+           else ttp.job
+           end,
+       case
+           when ttp."characters" = '\N' then null
+           else ttp."characters"
+           end
+from temp_title_principals ttp;
 
 drop table if exists temp_title_principals;
 
@@ -317,6 +329,7 @@ drop table if exists temp_title_ratings;
 
 -- References
 -- How to install and run postgres in a docker container https://betterprogramming.pub/connect-from-local-machine-to-postgresql-docker-container-f785f00461a7
+-- docker start my-postgres
 -- Where to download IMDB datasets https://datasets.imdbws.com/
 -- Extract.gz files https://stackoverflow.com/a/50996888/1577363
 -- Copy files from local to docker container https://support.sitecore.com/kb?id=kb_article_view&sysparm_article=KB0383441
