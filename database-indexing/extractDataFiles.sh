4ae878bec02095ebd850e95e3dadb083582fa83d
Erhun Baycelik <1410256+muzir@users.noreply.github.com>
2022-01-16 00:01:06 +0200

    Add extract data shell (#131)
    Co-authored-by: muzir <>

---
+++ database-indexing/extractDataFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+gunzip name.basics.tsv.gz
+gunzip title.akas.tsv.gz
+gunzip title.basics.tsv.gz
+gunzip title.crew.tsv.gz
+gunzip title.episode.tsv.gz
+gunzip title.principals.tsv.gz
+gunzip title.ratings.tsv.gz
