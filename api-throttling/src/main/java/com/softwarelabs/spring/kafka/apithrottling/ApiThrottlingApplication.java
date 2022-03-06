7d156663e869f34e6df4adacee396943cc91deb5
muzir <>
2022-03-06 13:25:28 +0200

    Create api-throttling project

---
+++ api-throttling/src/main/java/com/softwarelabs/spring/kafka/apithrottling/ApiThrottlingApplication.java
@@ -0,0 +1,13 @@
+package com.softwarelabs.spring.kafka.apithrottling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiThrottlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiThrottlingApplication.class, args);
+	}
+
+}
