b2e6b8ef9398c156c793cd8cd4a57df64b736d0d
muzir <>
2022-05-31 22:29:54 +0300

    Update queries

--- database-indexing/stackoverflow/compositeIndex.sql
+++ database-indexing/stackoverflow/compositeIndex.sql
@@ -1,13 +1,12 @@
 select count(1) from posts;
 
-select * from posts limit 10;
-
-select * from posts where owneruserid = 1 AND creationdate between '2007-01-01' and '2008-01-01';
-
-select * from posts where owneruserid = 1 AND creationdate > '2007-01-01' and creationdate < '2008-01-01';
-
 select * from posts where owneruserid = 1 AND creationdate > '2010-01-01' and creationdate < '2011-01-01';
 
-explain analyse select * from posts where owneruserid = 2 AND creationdate > '2010-01-01' and creationdate < '2011-01-01';
+explain analyse select * from posts where owneruserid = 4 AND creationdate > '2010-01-01' and creationdate < '2011-01-01';
+explain analyse select * from posts where owneruserid = 31312432;
 
+create index if not exists idx_owneruserid_on_posts on posts(owneruserid);
 create index if not exists idx_owneruserid_creationdate_on_posts on posts(owneruserid, creationdate);
+
+drop index if exists idx_owneruserid_creationdate_on_posts;
+drop index if exists idx_owneruserid_on_posts;
