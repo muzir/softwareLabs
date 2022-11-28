3c8a5b18552d77cab0a40aa1f71e457b0cc3be50
muzir <>
2022-11-28 23:07:27 +0200

    Downgrade version changes

--- spring-boot-integration-test/src/main/java/com/softwarelabs/product/IProductPort.java
+++ spring-boot-integration-test/src/main/java/com/softwarelabs/product/IProductPort.java
@@ -1,43 +1,52 @@
 package com.softwarelabs.product;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 
 public interface IProductPort {
 
-	@PostMapping(
-			value = "/v1/product",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
+    @PostMapping(
+            value = "/v1/product",
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @ResponseBody
+    ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
 
-	@GetMapping(
-			value = "/v1/product/{productId}",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse getProductById(@PathVariable("productId") Long productId);
+    @GetMapping(
+            value = "/v1/product/{productId}",
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @ResponseBody
+    ProductResponse getProductById(@PathVariable("productId") Long productId);
 
-	@Data
-	@Accessors(chain = true) class ProductResponse {
-		ProductDto product;
-		Result result;
-	}
+    @Data
+    @Accessors(chain = true)
+    class ProductResponse {
+        ProductDto product;
+        Result result;
+    }
 
-	@Data
-	@Accessors(chain = true) class Result {
-		private boolean success;
-		private String message;
-	}
+    @Data
+    @Accessors(chain = true)
+    class Result {
+        private boolean success;
+        private String message;
+    }
 
-	@Data
-	@Accessors(chain = true) class ProductRequest {
-		@NotNull private Long id;
-		@NotNull
-		private String name;
-	}
+    @Data
+    @Accessors(chain = true)
+    class ProductRequest {
+        @NotNull
+        private Long id;
+        @NotNull
+        private String name;
+    }
 }
