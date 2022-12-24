f2c14fabfb09b2973b4473795f48cf121e3897bb
muzir <>
2022-12-24 16:41:29 +0200

    Upgrade from spring boot 2.7.5 to 3.0.1

--- spring-boot-containers/src/main/java/com/softwarelabs/product/PersistantProduct.java
+++ spring-boot-containers/src/main/java/com/softwarelabs/product/PersistantProduct.java
@@ -1,27 +1,28 @@
 package com.softwarelabs.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "product")
 public class PersistantProduct extends AbstractPersistable<Long> implements Product {
-	private String name;
+    private String name;
 
-	public PersistantProduct() {
-	}
+    public PersistantProduct() {
+    }
 
-	public PersistantProduct(String name) {
-		this.name = name;
-	}
+    public PersistantProduct(String name) {
+        this.name = name;
+    }
 
-	@Override public String name() {
-		return name;
-	}
+    @Override
+    public String name() {
+        return name;
+    }
 }
--- spring-boot-containers/src/main/java/com/softwarelabs/product/ProductPort.java
+++ spring-boot-containers/src/main/java/com/softwarelabs/product/ProductPort.java
@@ -1,41 +1,48 @@
 package com.softwarelabs.product;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public interface ProductPort {
 
-	@PostMapping(
-			value = "/v1/product",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
+    @PostMapping(
+            value = "/v1/product",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
 
-	@GetMapping(
-			value = "/v1/product/{productName}",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse getProductByName(@PathVariable("productName") String productName);
+    @GetMapping(
+            value = "/v1/product/{productName}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    ProductResponse getProductByName(@PathVariable("productName") String productName);
 
-	@Data
-	@Accessors(chain = true) class ProductResponse {
-		Product product;
-		Result result;
-	}
+    @Data
+    @Accessors(chain = true)
+    class ProductResponse {
+        Product product;
+        Result result;
+    }
 
-	@Data
-	@Accessors(chain = true) class Result {
-		private boolean success;
-		private String message;
-	}
+    @Data
+    @Accessors(chain = true)
+    class Result {
+        private boolean success;
+        private String message;
+    }
 
-	@Data
-	@Accessors(chain = true) class ProductRequest {
-		@NotNull private String name;
-	}
+    @Data
+    @Accessors(chain = true)
+    class ProductRequest {
+        @NotNull
+        private String name;
+    }
 }
