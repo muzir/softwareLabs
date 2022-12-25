10bb3ecb9f7f86cc1309fe740ef97b3178f746fc
muzir <>
2022-12-25 23:01:20 +0200

    Upgrade spring boot 3

--- spring-boot-kafka/src/main/java/com/softwarelabs/product/ProductPort.java
+++ spring-boot-kafka/src/main/java/com/softwarelabs/product/ProductPort.java
@@ -1,80 +1,84 @@
 package com.softwarelabs.product;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public interface ProductPort {
 
-	@PostMapping(
-			value = "/v1/product",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
+    @PostMapping(
+            value = "/v1/product",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    ProductResponse createProduct(@RequestBody @Valid ProductRequest request);
 
-	@GetMapping(
-			value = "/v1/product/{productName}",
-			produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-	@ResponseBody
-	ProductResponse getProductByName(@PathVariable("productName") String productName);
+    @GetMapping(
+            value = "/v1/product/{productName}",
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    ProductResponse getProductByName(@PathVariable("productName") String productName);
 
-	@Getter
-	class ProductResponse {
-		private Product product;
-		private Result result;
+    @Getter
+    class ProductResponse {
+        private Product product;
+        private Result result;
 
-		public ProductResponse(Product product, Result result) {
-			this.product = product;
-			this.result = result;
-		}
+        public ProductResponse(Product product, Result result) {
+            this.product = product;
+            this.result = result;
+        }
 
-	}
+    }
 
-	@Getter
-	class Result {
-		private boolean success;
-		private String message;
+    @Getter
+    class Result {
+        private boolean success;
+        private String message;
 
-		public Result(boolean success, String message) {
-			this.success = success;
-			this.message = message;
-		}
+        public Result(boolean success, String message) {
+            this.success = success;
+            this.message = message;
+        }
 
-	}
+    }
 
-	class ProductRequest implements Product {
-		@NotNull
-		private String name;
-		@NotNull
-		private BigDecimal price;
+    class ProductRequest implements Product {
+        @NotNull
+        private String name;
+        @NotNull
+        private BigDecimal price;
 
-		public ProductRequest(String name) {
-			this.name = name;
-			this.price = BigDecimal.ZERO;
-		}
+        public ProductRequest(String name) {
+            this.name = name;
+            this.price = BigDecimal.ZERO;
+        }
 
-		public ProductRequest(@NotNull String name, @NotNull BigDecimal price) {
-			this.name = name;
-			this.price = price;
-		}
+        public ProductRequest(@NotNull String name, @NotNull BigDecimal price) {
+            this.name = name;
+            this.price = price;
+        }
 
-		@Override
-		public Long id() {
-			return null;
-		}
+        @Override
+        public Long id() {
+            return null;
+        }
 
-		@Override
-		public String name() {
-			return name;
-		}
+        @Override
+        public String name() {
+            return name;
+        }
 
-		@Override
-		public BigDecimal price() {
-			return price;
-		}
-	}
+        @Override
+        public BigDecimal price() {
+            return price;
+        }
+    }
 }
--- spring-boot-kafka/src/main/java/com/softwarelabs/product/ProductProducer.java
+++ spring-boot-kafka/src/main/java/com/softwarelabs/product/ProductProducer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softwarelabs.kafka.EventProducer;
 import com.softwarelabs.kafka.KafkaProducerFactory;
 import com.softwarelabs.kafka.KafkaTopicNames;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
@@ -13,67 +14,66 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PreDestroy;
 
 @Slf4j
 @Service
 public class ProductProducer implements EventProducer<String> {
 
-	private final Producer<String, String> kafkaProducer;
+    private final Producer<String, String> kafkaProducer;
 
-	private final ObjectMapper mapper;
+    private final ObjectMapper mapper;
 
-	private final Callback produceCallback = new ProduceCallback();
+    private final Callback produceCallback = new ProduceCallback();
 
-	@Autowired
-	public ProductProducer(KafkaProducerFactory<String, String> kafkaProducerFactory, ObjectMapper mapper) {
-		this.kafkaProducer = kafkaProducerFactory.createProducer(this.producerClientId());
-		this.mapper = mapper;
-	}
+    @Autowired
+    public ProductProducer(KafkaProducerFactory<String, String> kafkaProducerFactory, ObjectMapper mapper) {
+        this.kafkaProducer = kafkaProducerFactory.createProducer(this.producerClientId());
+        this.mapper = mapper;
+    }
 
-	public void publishProductChange(Product product) throws JsonProcessingException {
-		ProductChange productChange = new ProductChange(product.name(), product.price());
-		String productChangeMessage = mapper.writeValueAsString(productChange);
-		publish(productChangeMessage);
-	}
+    public void publishProductChange(Product product) throws JsonProcessingException {
+        ProductChange productChange = new ProductChange(product.name(), product.price());
+        String productChangeMessage = mapper.writeValueAsString(productChange);
+        publish(productChangeMessage);
+    }
 
-	@Override
-	public void publish(String message) {
-		ProducerRecord<String, String> record = new ProducerRecord<>(topicName(), "1", message);
-		log.info("Publish topic : {} message : {}", topicName(), message);
-		kafkaProducer.send(record, produceCallback);
-	}
+    @Override
+    public void publish(String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topicName(), "1", message);
+        log.info("Publish topic : {} message : {}", topicName(), message);
+        kafkaProducer.send(record, produceCallback);
+    }
 
-	@Override
-	public String topicName() {
-		return KafkaTopicNames.PRODUCT_CHANGE_TOPIC;
-	}
+    @Override
+    public String topicName() {
+        return KafkaTopicNames.PRODUCT_CHANGE_TOPIC;
+    }
 
-	@Override
-	public String producerClientId() {
-		return "productClientId";
-	}
+    @Override
+    public String producerClientId() {
+        return "productClientId";
+    }
 
-	@PreDestroy
-	@Override
-	public void close() {
-		kafkaProducer.flush();
-		kafkaProducer.close();
-	}
+    @PreDestroy
+    @Override
+    public void close() {
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
 
-	private class ProduceCallback implements Callback {
+    private class ProduceCallback implements Callback {
 
-		@Override
-		public void onCompletion(RecordMetadata metadata, Exception exception) {
-			if (exception != null) {
-				log.error("Message can't be sent", exception);
-				return;
-			}
-			log.info("Producer topic {}, partition {}, offset {}, messageArrivedTime",
-					metadata.topic(),
-					metadata.partition(),
-					metadata.offset(),
-					metadata.timestamp());
-		}
-	}
+        @Override
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+            if (exception != null) {
+                log.error("Message can't be sent", exception);
+                return;
+            }
+            log.info("Producer topic {}, partition {}, offset {}, messageArrivedTime",
+                    metadata.topic(),
+                    metadata.partition(),
+                    metadata.offset(),
+                    metadata.timestamp());
+        }
+    }
 }
