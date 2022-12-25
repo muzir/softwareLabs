87508fff58e3d8e3b0c1fcec710bfea8f8ffc19e
muzir <>
2022-12-25 13:52:28 +0200

    Upgrade spring 2.7.7 and delete hibernate

--- spring-boot-kafka/src/main/java/com/softwarelabs/product/PersistantProduct.java
+++ spring-boot-kafka/src/main/java/com/softwarelabs/product/PersistantProduct.java
@@ -1,49 +1,52 @@
 package com.softwarelabs.product;
 
 import lombok.Getter;
-import org.springframework.data.jpa.domain.AbstractPersistable;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Random;
 
 @Getter
-@Entity
-@Table(name = "product")
-public class PersistantProduct extends AbstractPersistable<Long> implements Product {
+@Setter
+@NoArgsConstructor
+public class PersistantProduct implements Product {
 
-	private String name;
-	private BigDecimal price;
+    private String name;
+    private BigDecimal price;
+    private Long id;
 
-	public PersistantProduct() {
-	}
+    public PersistantProduct(String name) {
+        this.id = new Random().nextLong();
+        this.name = name;
+        this.price = BigDecimal.ZERO;
+    }
 
-	public PersistantProduct(String name) {
-		this.name = name;
-		this.price = BigDecimal.ZERO;
-	}
+    public PersistantProduct(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
-	public PersistantProduct(Long id, String name, BigDecimal price) {
-		this.setId(id);
-		this.name = name;
-		this.price = price;
-	}
+    public PersistantProduct(Product product) {
+        this.id = Optional.ofNullable(product.id()).orElse(new Random().nextLong());
+        this.name = product.name();
+        this.price = product.price();
+    }
 
-	public PersistantProduct(Product product) {
-		this.setId(product.id());
-		this.name = product.name();
-		this.price = product.price();
-	}
+    @Override
+    public Long id() {
+        return id;
+    }
 
-	@Override public Long id() {
-		return getId();
-	}
+    @Override
+    public String name() {
+        return name;
+    }
 
-	@Override public String name() {
-		return name;
-	}
-
-	@Override public BigDecimal price() {
-		return price;
-	}
+    @Override
+    public BigDecimal price() {
+        return price;
+    }
 }
